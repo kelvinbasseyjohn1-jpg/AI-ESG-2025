a71dceb973a642f13769e93d6e4e089f7c80b851
kelvinbasseyjohn1-jpg <kelvinbasseyjohn1@gmail.com>
2025-12-30 06:34:36 +0100

    Add files via upload

---
+++ 01_data_cleaning.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_excel('data/raw/ONS_MES_2023.xlsx')
+
+# Example cleaning
+df = df.dropna(subset=['ai_indicator_1'])
+df.to_csv('data/cleaned/esg_ai_cleaned.csv', index=False)
